cab1c25cf1bd515734e287c715a822524081b966
Herley Antoine <ffast2000@gmail.com>
2025-07-22 13:22:56 -0400

    feat(cart): integrate translations for DrawerTitle in CartDialog component

--- components/shared/cart/cart-aside.tsx
+++ components/shared/cart/cart-aside.tsx
@@ -1,6 +1,7 @@
 import { Drawer, DrawerContent, DrawerTitle } from "@/components/ui/drawer";
 import { useCartModal } from "@/context/cart-modal";
 import useMediaQuery from "@/hook/useMediaquery";
+import { useTranslations } from "next-intl";
 
 export default function CartDialog({
   children,
@@ -9,13 +10,14 @@ export default function CartDialog({
 }) {
   const { isOpen, setIsOpen } = useCartModal();
   const isDesktop = useMediaQuery("(min-width: 768px)");
+  const t = useTranslations("");
   return (
     <Drawer
       open={isOpen}
       shouldScaleBackground={true}
       direction={isDesktop ? "right" : "bottom"}
     >
-      <DrawerTitle className="sr-only">Shopping cart</DrawerTitle>
+      <DrawerTitle className="sr-only">{t("")}</DrawerTitle>
       <DrawerContent
         className="sm:fixed sm:bottom-0 sm:left-auto sm:right-0 sm:top-0 sm:mt-0 sm:flex sm:h-full sm:w-1/2 sm:flex-col sm:overflow-hidden sm:rounded-none sm:bg-white sm:shadow-xl lg:w-1/3"
         aria-describedby="cart-overlay-description"
