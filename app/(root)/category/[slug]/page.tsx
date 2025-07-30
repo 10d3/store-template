c467d0fc738c4f5ec725fde8c8827709f8c27344
Herley Antoine <ffast2000@gmail.com>
2025-07-30 10:24:44 -0400

    refactor(category): convert page component to async and add slug logging
    Add async/await to handle promise-based params and log slug for debugging purposes

--- app/(root)/category/[slug]/page.tsx
+++ app/(root)/category/[slug]/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+export default async function page({
+  props,
+}: {
+  props: { params: Promise<{ slug: string }> };
+}) {
+  const params = await props.params;
+  console.log(params.slug);
+
+  return <div>page</div>;
 }
