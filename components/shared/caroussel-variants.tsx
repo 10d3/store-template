50d1823db62628b359a243b8e14f6faf11976b29
Herley Antoine <ffast2000@gmail.com>
2025-08-04 12:44:20 -0400

    feat(carousel): enhance CarousselVariants with image display and layout adjustments
    - Add Image component to display product images in CarousselVariants
    - Update CarouselContent layout to use a grid format
    - Refactor button components for improved styling and cursor behavior

--- components/shared/caroussel-variants.tsx
+++ components/shared/caroussel-variants.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { StripeProduct } from "@/types/product";
 import React, { useEffect, useState } from "react";
 import {
@@ -9,6 +9,7 @@ import {
 } from "../ui/carousel";
 import { Button } from "../ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
 
 export default function CarousselVariants({
   products,
@@ -29,21 +30,36 @@ export default function CarousselVariants({
 
   return (
     <Carousel className="w-full" setApi={setApi}>
-      <CarouselContent>
+      <CarouselContent className="grid grid-cols-3 gap-4 h-auto">
         {products.map((product) => (
           <CarouselItem key={product.id}>
-            {/*placeholder for card variant*/}
-            {product.name}
+            <div className="group overflow-hidden rounded-lg">
+              <div className="relative w-full h-full">
+                <Image
+                  width={1000}
+                  height={1000}
+                  className="w-full h-full object-cover transition-all duration-700 group-hover:scale-105"
+                  src={product.images?.[0] || ""}
+                  alt={product.name}
+                />
+              </div>
+            </div>
           </CarouselItem>
         ))}
       </CarouselContent>
       <div className="absolute top-1/2 left-0 transform -translate-y-1/2">
-        <Button onClick={() => api?.scrollTo(current - 1)}>
+        <Button
+          className="cursor-pointer rounded-full"
+          onClick={() => api?.scrollTo(current - 1)}
+        >
           <ChevronLeft />
         </Button>
       </div>
       <div className="absolute top-1/2 right-0 transform -translate-y-1/2">
-        <Button onClick={() => api?.scrollTo(current + 1)}>
+        <Button
+          className="cursor-pointer rounded-full"
+          onClick={() => api?.scrollTo(current + 1)}
+        >
           <ChevronRight />
         </Button>
       </div>
