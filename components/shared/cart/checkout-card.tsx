542e101773ef5b9da7f00d7e8f1cc7bcd49536f3
10d3 <ffast2000@gmail.com>
2025-11-10 21:06:04 -0500

    fix(ui): remove cart folder

--- components/shared/cart/checkout-card.tsx
+++ components/shared/cart/checkout-card.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import StripePayment from "./stripe-payment";
+// import StripePayment from "./stripe-payment";
 
 export default function CheckoutCard() {
   return (
     <section className="max-w-md pb-12">
       <h1>Checkout</h1>
-      <StripePayment />
+      {/* <StripePayment /> */}
     </section>
   );
 }
