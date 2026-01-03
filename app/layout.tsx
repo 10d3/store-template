c3b7dd63c46339cf861b86f4014b22400d4da630
10d3 <ffast2000@gmail.com>
2026-01-03 07:46:13 -0500

    feat: Initialize Next.js app with base layout, global styles, fonts, Uploadthing SSR, and generated Prisma client files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import { getLocale } from "next-intl/server";
 import Provider from "./provider";
 import { getLocale } from "@/i18n/server";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,7 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <Provider>{children}</Provider>
       </body>
     </html>
