b6416e14cbb80fd2752a3eb8d8ce42f3ee7962c7
10d3 <ffast2000@gmail.com>
2026-03-15 10:58:03 -0400

    feat: add cart drawer and empty state UI components

--- components/shared/cart/cart-aside.tsx
+++ components/shared/cart/cart-aside.tsx
@@ -2,7 +2,7 @@
 import { Drawer, DrawerContent, DrawerTitle } from "@/components/ui/drawer";
 import { useCartModal } from "@/context/cart-modal";
 import useMediaQuery from "@/hooks/useMediaquery";
-import { useTranslations } from "@/i18n/client";
+// import { useTranslations } from "@/i18n/client";
 
 export default function CartAsideDrawer({
   children,
@@ -11,14 +11,14 @@ export default function CartAsideDrawer({
 }) {
   const { isOpen, setIsOpen } = useCartModal();
   const isDesktop = useMediaQuery("(min-width: 768px)");
-  const t = useTranslations("cart");
+  // const t = useTranslations("cart");
   return (
     <Drawer
       open={isOpen}
       shouldScaleBackground={true}
       direction={isDesktop ? "right" : "bottom"}
     >
-      <DrawerTitle className="sr-only">{t("title")}</DrawerTitle>
+      <DrawerTitle className="sr-only">Shopping Cart</DrawerTitle>
       <DrawerContent
         className="sm:fixed sm:bottom-0 sm:left-auto sm:right-0 sm:top-0 sm:mt-0 sm:flex sm:h-full sm:w-1/2 sm:flex-col sm:overflow-hidden sm:rounded-none sm:shadow-xl lg:w-1/3"
         aria-describedby="cart-overlay-description"
